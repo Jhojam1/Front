1337810ccf0435258ca3ed325e4a11b3b864ac4a
Jhojam1 <JhojamCaraballo@gmail.com>
2024-10-29 15:26:10 -0500

    Se Agrega ventana modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,27 @@
 // Header.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
+import Modal from './Modal.jsx';
 
 function Header() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOpenModal = () => setIsModalOpen(true);
+    const handleCloseModal = () => setIsModalOpen(false);
+
     return (
         <header className="header">
             <div className="logo">
                 <img src="src/images/PQRSmart-Logo.png" alt="PQRSmart Logo"/>
             </div>
             <nav className="nav">
-            <a href="#inicio" className="nav-link">Inicio</a>
+                <a href="#inicio" className="nav-link">Inicio</a>
                 <a href="#como-funciona" className="nav-link">Cómo Funciona</a>
-                <a href="#crear-solicitud" className="nav-link">Crear Solicitud</a>
+                <a href="#crear-solicitud" className="nav-link" onClick={handleOpenModal}>Crear Solicitud</a>
                 <a href="#contacto" className="nav-link">Contacto</a>
                 <button className="header-login-btn">Iniciar Sesión</button>
             </nav>
+            <Modal isOpen={isModalOpen} onClose={handleCloseModal} />
         </header>
     );
 }
